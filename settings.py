4c1c49545b8b82b88c8853a7eafdffacf5337925
ebalders <erikb@datarg.com>
2012-05-19 16:15:50 -0300

    Update settings.py

--- settings.py
+++ settings.py
@@ -7,18 +7,18 @@ debug_profiler_enabled = False
 appname = 'WebPutty OSS'
 appversion_raw = [1, 3, 0]
 appversion = '.'.join([str(num) for num in appversion_raw])
-invite_sender_email = '%s Invitation <you@example.com>' % appname
-incoming_sender_email = '%s Incoming Mail <you@example.com>' % appname
+invite_sender_email = '%s Invitation <erikb@datarg.com>' % appname
+incoming_sender_email = '%s Incoming Mail <erikb@datarg.com>' % appname
 log_all_incoming = True
 # List of admins to forward mail to.
-forward_mail_to = ['you@example.com']
+forward_mail_to = ['erikb@datarg.com']
 jquery_url = '//ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js'
 # Generate this once by calling os.urandom(24)
-secret_key = "Shhh... It's a secret."
+secret_key = "p\xb8\xbcd\x0b\xc5\xab\xfd$\xe7\xc8uY\x923\x1c\xe2\xcd\xf5\xf4\xa8d\xad{"
 
 # Name of the Google Cloud Storage bucket
-use_google_cloud_storage = False
-google_bucket = 'yourbucket'
+use_google_cloud_storage = True
+google_bucket = 'webputtyerikb'
 
 available_locales = [
     ('en', u'English'),
@@ -27,7 +27,7 @@ available_locales = [
 
 # API Keys for url2png.com
 url2png = dict(
-    user = 'USERNAME',
-    password = 'PASSWORD',
+    user = 'ebalders',
+    password = 'Stopgate1717',
     bounds = '300x300',
 )
